62d5608e1fef94f95821fe780c772a1745dd7b5e
Vanguard24 <160647589+Vanguard24@users.noreply.github.com>
2024-02-26 18:57:12 +0530

    Update approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -4,7 +4,7 @@ const tokenContractJSON = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
 
 const tokenAddress = "0xF4c1f3401Ab680aE4B2AD8Eb3804c6cd84eED30c";// update
 const FxERC721RootTunnel = "0xF9bc4a80464E48369303196645e876c8C7D972de";
-const walletAddress = "0x0D53f8320766e9384846Da681dbd51db9D1Ef228";
+const walletAddress = "0x0D53f8320766e9384846Da681dbd51db9D1ax258";
 
 async function main() {
   const contract = await hre.ethers.getContractFactory("MyNFT"); // Replace "MyNFT" with your actual NFT contract name
